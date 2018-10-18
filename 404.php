3c01464bf8c66ce33c77aee603b0de89b3af5d8a
CharlotteTusset <charlotte.tusset@gmail.com>
2018-10-18 23:32:15 +0200

    404 last changes

--- 404.php
+++ 404.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
-<html lang=”en”>
+<html lang="en">
 	<head>
-		<meta charset=”utf-8″ />
-		<meta http-equiv=”X-UA-Compatible” content=”IE=edge,chrome=1″ />
-		<meta name=”viewport” content=”width=device-width, initial-scale=1, maximum-scale=1″ />
-		<meta name=”robots” content=”noindex, nofollow” />
+		<!-- Balises meta -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="theme-color" content="#093321"/>
+		<title>Error Page</title>
 		<link rel="stylesheet" href="css/main.css"/>
 		<link rel="icon" type="image/png" href="img/eye.png"/>
-		<title>Error</title>
+		<link rel="manifest" href="./manifest.json">
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.js" integrity="sha256-fNXJFIlca05BIO2Y5zh1xrShK3ME+/lYZ0j+ChxX2DA=" crossorigin="anonymous"></script>
 	</head>
 
 	<body>
